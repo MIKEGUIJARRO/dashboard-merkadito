64f24b0e868fd1e279cac71b5dbc078b2dba5e87
Mike Guijarro <a01612042@itesm.mx>
2021-04-05 22:22:21 -0400

    toggle implemented

--- src/components/Seller/Dashboard/Sidebar.js
+++ src/components/Seller/Dashboard/Sidebar.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import "react-toggle/style.css"
+import Toggle from 'react-toggle';
+
 import { Save } from 'react-feather';
 
 import { Text } from '../../UI/Button/Text'
@@ -13,11 +16,13 @@ export const Sidebar = () => {
                 <input type="text" name="name" placeholder="Name" className="px-2 py-1 rounded-lg" />
                 <input type="number" name="price" placeholder="$12.00" className="px-2 py-1 rounded-lg" />
                 <div>
-                    <input type="checkbox" name="status" placeholder="status"
-                    />
-                    <label for="vehicle1"> status</label>
-
+                    <label className="inline-flex items-center space-x-2">
+                        <Toggle className="" />
+                        <span>Active</span>
+                    </label>
                 </div>
+
+
                 <input className="max-w-min" type="file" accept="image/*" name="image" placeholder="image" className="px-2 py-1 rounded-lg" />
                 <Text logo={<Save />} text="texto" />
             </form>
