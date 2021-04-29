bb3f048e1c45e046da6e83db74a7112a3c0ad8c4
Mike <a01612042@itesm.mx>
2021-04-29 15:52:42 -0400

    useReducer initial implementation

--- src/screens/Seller/Dashboard.js
+++ src/screens/Seller/Dashboard.js
@@ -6,6 +6,9 @@ import { Body } from '../../components/Seller/Dashboard/Body';
 import { Sidebar } from '../../components/Seller/Dashboard/Sidebar';
 
 export const Dashboard = () => {
+
+    const [state, dispatch] = useReducer(reducer, initialState, init)
+
     return (
         <div className="flex flex-row">
             <div className="color-light-bg p-6 max-w-max flex flex-col space-y-5 h-screen sticky top-0 overflow-y-auto sticky max-h-screen min-w-min">
